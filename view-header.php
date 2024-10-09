02895e74acecf06512c81fc2af79c79d65756a1c
Shohruz2001 <123128209+Shohruz2001@users.noreply.github.com>
2024-10-09 13:21:46 -0500

    Create view-header.php

---
+++ view-header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $pageTitle ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgICS+VKNBQNGCHeKRQN+PtmoHDEXuppvnDIzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="post.php">Post</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="post-result.php">Post result</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="get.php">Get</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="get-result.php">Get result</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
