9aed25841a095c97bfdd33ff43a6621cb1185cb7
Shohruz2001 <123128209+Shohruz2001@users.noreply.github.com>
2024-10-09 15:11:37 -0500

    Create view-cars.php

---
+++ view-cars.php
@@ -0,0 +1,37 @@
+<h1>Cars</h1>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Owner ID</th>
+                <th>Make</th>
+                <th>Model</th>
+                <th>Year</th>
+                <th>License Plate</th>
+                <th>Location</th>
+                <th>Availability Start</th>
+                <th>Availability End</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        while ($car = $cars->fetch_assoc()) {
+        ?>
+            <tr>
+                <td><?php echo $car['car_id']; ?></td>
+                <td><?php echo $car['owner_id']; ?></td>
+                <td><?php echo $car['make']; ?></td>
+                <td><?php echo $car['model']; ?></td>
+                <td><?php echo $car['year']; ?></td>
+                <td><?php echo $car['license_plate']; ?></td>
+                <td><?php echo $car['location']; ?></td>
+                <td><?php echo $car['availability_start']; ?></td>
+                <td><?php echo $car['availability_end']; ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
