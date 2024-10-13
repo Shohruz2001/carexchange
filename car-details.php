33ae533dcdae12941914a4bf67d1350126265566
Shohruz2001 <123128209+Shohruz2001@users.noreply.github.com>
2024-10-12 21:55:21 -0500

    Update car-details.php

--- car-details.php
+++ car-details.php
@@ -1,6 +1,7 @@
 <?php
 require_once("util-db.php");
 
+// Fetch car details and related reservations
 function getCarDetails($car_id) {
     try {
         $conn = get_db_connection();
@@ -16,8 +17,25 @@ function getCarDetails($car_id) {
     }
 }
 
+// Fetch reservations associated with the car
+function getCarReservations($car_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM reservations WHERE car_id = ?");
+        $stmt->bind_param("i", $car_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 if (isset($_GET['id'])) {
     $car = getCarDetails($_GET['id']);
+    $reservations = getCarReservations($_GET['id']);
 } else {
     echo "Car ID not provided!";
     exit;
@@ -34,5 +52,27 @@ include "view-header.php";
 <p>License Plate: <?php echo $car['license_plate']; ?></p>
 <p>Location: <?php echo $car['location']; ?></p>
 <p>Availability: <?php echo $car['availability_start']; ?> to <?php echo $car['availability_end']; ?></p>
+
+<h2>Reservations for this car</h2>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Reservation ID</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php while ($reservation = $reservations->fetch_assoc()) { ?>
+        <tr>
+            <td><?php echo $reservation['reservation_id']; ?></td>
+            <td><?php echo $reservation['start_date']; ?></td>
+            <td><?php echo $reservation['end_date']; ?></td>
+            <td><?php echo $reservation['status']; ?></td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
 
 <?php include "view-footer.php"; ?>
