c9c681bf1b4eb5792a3e88751a24bcbc875de533
Shohruz2001 <123128209+Shohruz2001@users.noreply.github.com>
2024-12-12 19:31:16 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -32,22 +32,17 @@
         .container {
             max-width: 90%; /* To prevent the content from being too wide */
         }
-
-        /* Center navigation links */
-        .navbar-nav {
-            margin: auto;
-        }
     </style>
 </head>
 <body>
     <div class="container mt-4">
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav mx-auto">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
